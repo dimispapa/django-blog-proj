f11da5a092e08765cd23b821b895e028709708f9
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-08-26 13:04:17 +0000

    feat: add uploading feature for about profile img

--- blog/models.py
+++ blog/models.py
@@ -11,8 +11,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="blog_posts")
-    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
